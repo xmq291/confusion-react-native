040fcf73264fc1e87f0f6a293056822017ac285b
xmq291 <xmq291@gmail.com>
2018-12-18 14:52:32 -0500

    Buttons and Icons Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,9 +6,14 @@ import Contact from './ContactComponent'
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { Icon } from "react-native-elements";
 
 const MenuNavigator = createStackNavigator({
-    Menu: { screen: Menu },
+    Menu: { screen: Menu,
+        navigationOptions: ({navigation}) => ({
+            headerLeft: <Icon name='menu' size={24} color='white' 
+            onPress={() => navigation.toggleDrawer()} />
+        })},
     Dishdetail: { screen: Dishdetail }
 }, {
     initialRouteName: 'Menu',
@@ -26,43 +31,49 @@ const MenuNavigator = createStackNavigator({
 const HomeNavigator = createStackNavigator({
     Home: { screen: Home },
 }, {
-    navigationOptions: {
+    navigationOptions: ({navigation}) => ({
         headerStyle: {
             backgroundColor: '#512DA8'
         },
         headerTintColor: '#fff',
         headerBackTitleStyle: {
             color: '#fff'
-        }
-    }
+        },
+        headerLeft: <Icon name='menu' size={24} color='white' 
+        onPress={() => navigation.toggleDrawer()} />
+    })
 });
 
 const AboutNavigator = createStackNavigator({
     About: { screen: About },
 }, {
-    navigationOptions: {
+    navigationOptions: ({navigation}) => ({
         headerStyle: {
             backgroundColor: '#512DA8'
         },
         headerTintColor: '#fff',
         headerBackTitleStyle: {
             color: '#fff'
-        }
-    }
+        },
+        headerLeft: <Icon name='menu' size={24} color='white' 
+        onPress={() => navigation.toggleDrawer()} />
+    })
 });
 
 const ContactNavigator = createStackNavigator({
     Contact: { screen: Contact },
 }, {
-    navigationOptions: {
+    navigationOptions: ({navigation}) => ({
         headerStyle: {
             backgroundColor: '#512DA8'
         },
         headerTintColor: '#fff',
         headerBackTitleStyle: {
             color: '#fff'
-        }
-    }
+        },
+        headerLeft: <Icon name='menu' size={24} color='white' 
+        onPress={() => navigation.toggleDrawer()} />
+    })
 });
 
 const MainNavigator = createDrawerNavigator({
@@ -70,28 +81,60 @@ const MainNavigator = createDrawerNavigator({
         screen: HomeNavigator,
         navigationOptions: {
             title: 'Home',
-            drawerLabel: 'Home'
+            drawerLabel: 'Home',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                    name='home' 
+                    type='font-awesome' 
+                    size={24} 
+                    color={tintColor}
+                />
+            )
         }
     },
     About: { 
         screen: AboutNavigator,
         navigationOptions: {
             title: 'About Us',
-            drawerLabel: 'About Us'
+            drawerLabel: 'About Us',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                    name='info-circle' 
+                    type='font-awesome' 
+                    size={24} 
+                    color={tintColor}
+                />
+            )
         }
     },
     Menu: { 
         screen: MenuNavigator,
         navigationOptions: {
             title: 'Menu',
-            drawerLabel: 'Menu'
+            drawerLabel: 'Menu',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                    name='list' 
+                    type='font-awesome' 
+                    size={24} 
+                    color={tintColor}
+                />
+            )
         }
     },
     Contact: { 
         screen: ContactNavigator,
         navigationOptions: {
             title: 'Contact Us',
-            drawerLabel: 'Contact Us'
+            drawerLabel: 'Contact Us',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                    name='address-card' 
+                    type='font-awesome' 
+                    size={22} 
+                    color={tintColor}
+                />
+            )
         }
     }
 }, {
