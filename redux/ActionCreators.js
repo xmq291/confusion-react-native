fc118dffc8e99ce73f4d6613435cf5b8e44af831
xmq291 <xmq291@gmail.com>
2018-12-24 17:12:52 -0500

    Redux Adding Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -145,3 +145,14 @@ export const addLeaders = (leaders) => ({
     type: ActionTypes.ADD_LEADERS,
     payload: leaders
 });
+
+export const postFavorite = (dishId) => (dispatch) => {
+    setTimeout(() => {
+        dispatch(addFavrorite(dishId));
+    }, 2000);
+}
+
+export const addFavrorite = (dishId) => ({
+    type: ActionTypes.ADD_FAVORITE,
+    payload: dishId
+});
